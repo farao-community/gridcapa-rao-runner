4d4a83fc575f698c0228776787159930e30cb8b4
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-01-25 14:31:48 +0100

    add unit test for metadata propagation throughout logs

--- rao-runner-app/src/test/java/com/farao_community/farao/rao_runner/app/RaoRunnerListenerTest.java
+++ rao-runner-app/src/test/java/com/farao_community/farao/rao_runner/app/RaoRunnerListenerTest.java
@@ -6,10 +6,14 @@
  */
 package com.farao_community.farao.rao_runner.app;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.farao_community.farao.rao_runner.api.resource.RaoRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
@@ -24,6 +28,8 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Mohamed BenRejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
@@ -70,5 +76,19 @@ class RaoRunnerListenerTest {
         Message message = MessageBuilder.withBody(inputMessage.getBytes()).build();
         raoRunnerListener.onMessage(message);
         Mockito.verify(raoRunnerServer, Mockito.times(0)).runRao(Mockito.any(RaoRequest.class));
+    }
+
+    @Test
+    void checkThatMdcMetadataIsPropagatedCorrectly() {
+        Logger logger = (Logger) LoggerFactory.getLogger("LOGGER");
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
+        raoRunnerListener.addMetaDataToLogsModelContext("process-id", "request-id", "client-id");
+        logger.info("message");
+        assertEquals(3, listAppender.list.get(0).getMDCPropertyMap().size());
+        assertEquals("process-id", listAppender.list.get(0).getMDCPropertyMap().get("gridcapaTaskId"));
+        assertEquals("request-id", listAppender.list.get(0).getMDCPropertyMap().get("computationId"));
+        assertEquals("client-id", listAppender.list.get(0).getMDCPropertyMap().get("clientAppId"));
     }
 }
